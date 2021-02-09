05e7976c261495541a0ff658730a67ae7de0e6de
dhammer3 <daren@pop-os.localdomain>
2021-02-09 16:53:36 -0600

    updated to match pep8

--- test_sample.py
+++ test_sample.py
@@ -1,29 +1,22 @@
 import unittest
-from utils.youtube_crawler import youtube_crawler, WebDriver
-
-
-@classmethod
-def setUpClass(youtube_crawler):
-    wb = WebDriver()
-    yt = youtube_crawler(wb)
+from utils.YoutubeCrawler import YoutubeCrawler, WebDriver
 
 
 class TestYoutubeCrawler(unittest.TestCase):
-    
     def setUp(self):
         self.wb = WebDriver()
-        self.yt = youtube_crawler(self.wb)
-        self.video_url = 'https://www.youtube.com/watch?v=xCixkaXrVMI'
-        self.channel_url ='https://www.youtube.com/c/ImdadAhad/videos'
-    
+        self.yt = YoutubeCrawler(self.wb)
+        self.video_url = "https://www.youtube.com/watch?v=xCixkaXrVMI"
+        self.channel_url = "https://www.youtube.com/c/ImdadAhad/videos"
+
     def tearDown(self):
         del self.yt
-    
+
     def test_get(self):
         url = self.video_url
         self.yt.get(url)
         assert self.yt.url == url
-    
+
     def test_get_video_transcript(self):
         url = self.video_url
         self.yt.get(url)
@@ -36,31 +29,30 @@ class TestYoutubeCrawler(unittest.TestCase):
         url = self.channel_url
         self.yt.get(url)
         channel_videos = self.yt.get_list_of_videos_on_channel()
-        assert(len(channel_videos)!= 0)
-    
+        assert len(channel_videos) != 0
+
     def test_get_channel_sub_count(self):
         url = self.channel_url
         self.yt.get(url)
         subs = None
         subs = self.yt.get_channel_sub_count()
-        assert(subs != None)
-        
+        assert subs != None
+
     def test_get_video_view_count(self):
         url = self.video_url
         self.yt.get(url)
-        views =""
+        views = ""
         views = self.yt.get_video_view_count()
-        assert(type(views)==int)
-        
+        assert type(views) == int
+
     def test_get_recent_videos_from_query(self):
-        search_dict = self.yt.get_recent_videos_from_query('iceberg')
-        assert(search_dict != {})
-        
+        search_dict = self.yt.get_recent_videos_from_query("iceberg")
+        assert search_dict != {}
+
     # @unittest.skip("skipping")
     def test_get_all_comments_from_video(self):
         url = self.video_url
         self.yt.get(url)
-        comments={}
+        comments = {}
         comments = self.yt.get_all_comments_from_video()
-        assert(comments!={})
-        
+        assert comments != {}
